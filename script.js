18d1c62892471f0d8fc882662fd94f1463e50088
Kay Kelly <kaykelly88@gmail.com>
2023-03-05 22:14:06 +1300

    Added an user interface to the game

--- script.js
+++ script.js
@@ -3,14 +3,14 @@
 // Compare player and computer choice
 // Declare winner
 
-function getComputerChoice() {
+const getComputerChoice = () => {
     const arrayOfChoices = ['rock', 'paper', 'scissors']
     const randomNum = Math.floor(Math.random() * 3)
     const compChoice = arrayOfChoices[randomNum]
     return compChoice
 }
 
-function playRound(playerSelection, computerSelection) {
+const playRound = (playerSelection, computerSelection) => {
     if (playerSelection === computerSelection) {
         return "It's a tie"
     }
@@ -34,7 +34,57 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-let playerSelection = "rock"
-let computerSelection = getComputerChoice()
 
-console.log(playRound(playerSelection, computerSelection))
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+const container = document.querySelector('.container');
+const score = document.querySelector('.score');
+let playerScore = 0;
+let computerScore = 0;
+let round = 0;
+
+const play = (playerSelection) => {
+    const computerSelection = getComputerChoice();
+    const result = playRound(playerSelection, computerSelection);
+    const div = document.createElement('div');
+    div.classList.add('result');
+    div.textContent = result;
+    const previousResult = container.querySelector('.result');
+    if (previousResult) {
+      container.removeChild(previousResult);
+    }
+    container.appendChild(div);
+    if (result.includes('Player wins')) {
+        playerScore++;
+    } else if (result.includes('Computer wins')) {
+        computerScore++;
+    }
+    score.textContent = `Player: ${playerScore} Computer: ${computerScore}`;
+    round++;
+    if (round === 5) {
+      if (playerScore > computerScore) {
+        alert(`Player ${playerScore} - ${computerScore} Computer. Player wins!`);
+      } else if (playerScore < computerScore) {
+        alert(`Player ${playerScore} - ${computerScore} Computer. Computer wins!`);
+      } else {
+        alert(`Player ${playerScore} - ${computerScore} Computer. Tie game..`);
+      }
+    }
+  }
+  
+  
+rock.addEventListener('click', () => {
+    let playerSelection = 'rock';
+    play(playerSelection);
+});
+
+paper.addEventListener('click', () => {
+    let playerSelection = 'paper';
+    play(playerSelection);
+});
+
+scissors.addEventListener('click', () => {
+    let playerSelection = 'scissors';
+    play(playerSelection);
+});
